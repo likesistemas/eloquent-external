1c6f31048225779124c46c85c7b3b42454e29a68
Ricardo Paes <ricardo@likesistemas.com.br>
2022-05-03 13:42:21 -0300

    💚 Increasing database column size

--- .docker/sql/database.sql
+++ .docker/sql/database.sql
@@ -1,14 +1,14 @@
 CREATE TABLE `produto` (
   `codigo` int(6) NOT NULL,
   `codigoSubcategoria` int(6) NOT NULL,
-  `nome` varchar(100) NOT NULL,
+  `nome` varchar(255) NOT NULL,
   `referencia` int(10) NULL DEFAULT NULL,
   `valor` decimal(10,2) NOT NULL DEFAULT 0.00
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `subcategoria` (
   `codigo` int(6) NOT NULL,
-  `nome` varchar(40) NOT NULL
+  `nome` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 ALTER TABLE `produto`
